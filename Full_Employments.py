46d66d5e382c5a73c46dd7af59de9ba800353e0e
paulo icaro <pauloicaro38@gmail.com>
2026-07-07 21:56:46 -0300

    Ajuste para empregos da Rais_Vinc. e inclusão da variável salário

--- Full_Employments.py
+++ Full_Employments.py
@@ -19,28 +19,33 @@ def full_employments(db, base_period):
     after = db['periodo'] > base_period       
         
         
-    # --- Check whether there is a matching db --- #
+    # --- Check where there is a matching db --- #
     if base.sum() == 0:
-        base_value = 0
+        base_value_empregos = 0
         db['flag_sem_rais'] = True
     else:
-        base_value = db.loc[base, 'estoque_empregos_rais'].iloc[0]
+        base_value_empregos = db.loc[base, 'estoque_empregos_rais_vinc'].iloc[0]
+        base_value_massa_salarial = db.loc[base, 'massa_salarial_rais_vinc'].iloc[0]
         db['flag_sem_rais'] = False
 
     # ------------------------ #
     # --- Flag Adjustments --- #
     # ------------------------ #
     
-    db['estoque_empregos'] = None
+    #db['estoque_empregos'] = None
     
     # --- Before --- #
-    db.loc[before, 'estoque_empregos'] = base_value - db.loc[before, 'saldo_empregos_caged'][::-1].cumsum()[::-1].shift(-1)
-
+    db.loc[before, 'estoque_empregos'] = base_value_empregos - db.loc[before, 'saldo_empregos_caged'][::-1].cumsum()[::-1].shift(-1)
+    db.loc[before, 'massa_salarial'] = base_value_massa_salarial - db.loc[before, 'massa_salarial_caged'][::-1].cumsum()[::-1].shift(-1)
     
     # --- Base --- #
-    db.loc[base, 'estoque_empregos'] = base_value
+    db.loc[base, 'estoque_empregos'] = base_value_empregos
+    db.loc[base, 'massa_salarial'] = base_value_massa_salarial
     
     # --- After --- #
-    db.loc[after, 'estoque_empregos'] = base_value + db.loc[after, 'saldo_empregos_caged'].cumsum()
+    db.loc[after, 'estoque_empregos'] = base_value_empregos + db.loc[after, 'saldo_empregos_caged'].cumsum()
+    db.loc[after, 'massa_salarial'] = base_value_massa_salarial + db.loc[after, 'massa_salarial_caged'].cumsum()
+    
+    db['salario_medio'] = db['massa_salarial']/db['estoque_empregos']
     
     return db
